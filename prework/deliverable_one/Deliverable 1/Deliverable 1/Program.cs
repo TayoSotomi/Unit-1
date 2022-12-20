50a459b31a03c0db1134fd2230880fe4764a0954
Tee <sotomi.tayo@gmail.com>
2022-12-20 07:19:32 -0600

    first commit for deliverable one

---
+++ prework/deliverable_one/Deliverable 1/Deliverable 1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+public class Program
+{
+    public static Void Main(string[] args)
+    {
+        
+        if (!contains)
+        {
+            {
+                Console.Write("How many people are we making PB&J sandwiches for?\n");
+            }
+            int input = int.Parse(Console.ReadLine());
+
+            {
+                Console.WriteLine(input * 2 + " Slices of bread");
+            }
+            {
+                Console.WriteLine(input * 2 + " tablespoons of peanut butter");
+            }
+            {
+                Console.WriteLine(input * 4 + " teaspoons of jelly\n");
+            }
+
+
+            double bread = Convert.ToDouble(input * 2 / 28);
+            double pb = Convert.ToDouble(input * 2 / 32);
+            double jelly = Convert.ToDouble(input * 4 / 48);
+
+            {
+                Console.WriteLine(Math.Round(bread, 2) + " loaves of bread");
+            }
+            { Console.WriteLine(Math.Round(pb, 2) + " jars of peanut butter"); }
+            {
+                Console.WriteLine(Math.Round(jelly, 2) + " jars of jelly");
+            }
+            Console.WriteLine();
+
+            {
+                String[] option = { "Y", "Yes" };
+
+                { Console.Write("Would you like to restart? Enter Y or Yes to continue, or any other key to exit.\n"); }
+
+                string recipeoption = Console.ReadLine();
+
+                bool contains = false;
+
+                for (int i = 0; i < option.Length; i++)
+                
+                    if (recipeoption == option[i])
+                    {
+                        contains = true;
+                    }
+                else
+            }
+                { Console.WriteLine("Goodbye!"); }
+        }
+        
+         }
+           
+        
+    }
+}         
+  
---
+++ prework/deliverable_two/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        
+        double PriceCoffee = 1.50;
+        double PriceWater = 0.5;
+
+        Console.Write("Hello welcome! What is your party size?\n");
+        int welcome = int.Parse(Console.ReadLine());
+
+        if (welcome >= 1 && welcome <= 6)
+        {
+
+            { Console.WriteLine("A table for " + welcome + " ! Please follow me and take a seat.Let's get everyone started with some drinks. We've got water or coffee.\n"); }
+
+
+            int Welcome = welcome;
+
+            for (Welcome = welcome; Welcome >= 1; Welcome--)
+            {
+                String[] drinks = { "water", "coffee" };
+
+                { Console.Write("Alright, water or coffee?\n"); }
+                String drinksOrder = Console.ReadLine();
+
+
+                bool menu = false;
+                for (int i = 0; i < drinks.Length; i++)
+                {
+                    if (drinksOrder == drinks[i])
+                    {
+                        menu = true;
+                    }
+
+                }
+                if (menu == true)
+                {
+                    int countCoffee = 0;
+                    for (int i = 1; i == drinks.Length; i += 1)
+                    {
+
+                        int CountCoffee = countCoffee += i;
+                    }
+
+                    int countWater = 0;
+                    for (int i = 0; i < drinks.Length; i += 1)
+                    { countWater = countWater += i; }
+
+
+                    double costCoffee = PriceCoffee * countCoffee;
+
+                    double costWater = PriceWater * countWater;
+
+                    double buffet = 9.99;
+
+                    if (Welcome <= 1)
+                    {
+
+                        Console.WriteLine();
+
+                        { Console.WriteLine("okay, so that's " + countCoffee + "  coffee(s) and " + countWater + "  bottle(s) of water. I'll be right back. Feel free to grab your food!\n"); }
+
+                        double total = buffet + costCoffee + costWater;
+
+                        Console.WriteLine();
+
+                        { Console.WriteLine("Here is your bill! Total price: $" + total); }
+                    }
+
+                }
+
+
+                else
+
+                {
+                    Console.WriteLine("Sorry, we dont have that. Do you care for something else");
+                }
+
+            }
+
+
+        }
+
+        else
+        {
+            Console.WriteLine("Oh sorry, can only seat parties up to 6. Have a nice day.\n");
+        }
+
+
+    }
+
+}
+
+
+
+
+
+
+
+
